2eb80d481a602fca4febe6091e3e5407b86d1fa5
Manish Sharma <manish.sharma0398@gmail.com>
2022-11-11 20:21:10 +0530

    added auth routes and controllers

---
+++ controllers/authController.js
@@ -0,0 +1,65 @@
+const bcrypt = require("bcryptjs");
+const { db } = require("../database/db");
+
+module.exports.register = (req, res) => {
+  const {
+    phone,
+    full_name,
+    email,
+    password,
+    profile_pic,
+    farm_name,
+    farm_address,
+  } = req.body;
+
+  const q = "SELECT * FROM users WHERE `email`=? ";
+
+  db.query(q, [email], (err, data) => {
+    if (err) return res.status(500).json(err);
+
+    if (data.length > 0)
+      return res.status(409).json({ message: "user already exists" });
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    const q =
+      "INSERT INTO users(`phone`, `full_name`, `email`, `password`, `profile_pic`, `farm_name`, `farm_address`) VALUES(?)";
+
+    const values = [
+      parseInt(phone),
+      full_name,
+      email,
+      hash,
+      profile_pic
+        ? profile_pic
+        : "https://thumbs.dreamstime.com/z/vector-illustration-isolated-white-background-user-profile-avatar-black-line-icon-user-profile-avatar-black-solid-icon-121102166.jpg",
+      farm_name,
+      farm_address,
+    ];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+
+      return res.status(200).json(data);
+    });
+  });
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+  const q = "SELECT * FROM users WHERE `email`=?";
+  db.query(q, [email], (err, data) => {
+    if (err) return res.json(err);
+
+    if (data.length === 0)
+      return res.status(404).json({ message: "No user found" });
+
+    const isPasswordCorrect = bcrypt.compareSync(password, data[0].password);
+
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Wrong login credentials" });
+
+    return res.status(200).json(data[0]);
+  });
+};
--- index.js
+++ index.js
@@ -5,10 +5,12 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
 const animalRoutes = require("./routes/animalRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 app.use(express.json());
 
 app.use("/api/animals", animalRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
