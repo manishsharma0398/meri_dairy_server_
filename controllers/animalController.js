1527bcd81ffe94c64c50223f46c1228428402b8d
Manish Sharma <manish.sharma0398@gmail.com>
2022-11-11 15:17:39 +0530

    adding routes for animals

--- controllers/animalController.js
+++ controllers/animalController.js
@@ -1,3 +1,19 @@
 exports.getAllAnimals = (req, res) => {
   res.json({ message: "Home page of animals" });
 };
+
+exports.getAnimalById = (req, res) => {
+  res.json({ message: "get animal by id" });
+};
+
+exports.createNewAnimal = (req, res) => {
+  res.json({ message: "add new animal" });
+};
+
+exports.updateAnimal = (req, res) => {
+  res.json({ message: "update animal" });
+};
+
+exports.deleteAnimal = (req, res) => {
+  res.json({ message: "animal deleted" });
+};
--- routes/animalRoutes.js
+++ routes/animalRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const animalControllers = require("../controllers/animalController");
 
-router.get("/", animalControllers.getAllAnimals);
+router.get("/all", animalControllers.getAllAnimals);
+router.get("/:aid", animalControllers.getAnimalById);
+router.post("/", animalControllers.createNewAnimal);
+router.put("/:aid", animalControllers.updateAnimal);
+router.delete("/:aid", animalControllers.deleteAnimal);
 
 module.exports = router;
